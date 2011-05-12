11c0f80b1aafc6a784dc0250b08163001307fdbc
Mihkel Sokk <mihkelsokk@gmail.com>
2011-05-12 09:38:30 +0000

    First test in C9

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var connect = require('connect'),
     util    = require('util'),
     Smog = require(__dirname + '/lib/smog'),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || process.env.C9_PORT || 3000;
 
 //Static server
 var server = connect.createServer(
@@ -16,7 +16,7 @@ server.listen(port);
 Smog.listen(server);
 
 
-if(!process.env.PORT) {
+if(!process.env.PORT && !process.env.C9_PORT) {
   var url = "http://127.0.0.1:" + port;
   util.log("Trying to start your browser in: " + url + "\n");
   require("child_process").exec("x-www-browser " + url);
