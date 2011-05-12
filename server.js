8330978de8155b9fdcc1d29ea6d227f96b37ffb4
Mihkel Sokk <mihkelsokk@gmail.com>
2011-05-12 12:54:23 +0300

    Fix

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var connect = require('connect'),
     Smog = require(__dirname + '/lib/smog'),
-<<<<<<< HEAD
     port = process.env.PORT || process.env.C9_PORT || 3000;
-=======
-    port = exports.port = process.env.PORT || 3000;
->>>>>>> 706fd55495135a8c84a331e275e241145decf878
 
 //Static server
 var server = connect.createServer(
@@ -15,7 +11,6 @@ var server = connect.createServer(
 server.listen(port);
 
 Smog.listen(server);
-<<<<<<< HEAD
 
 
 if(!process.env.PORT && !process.env.C9_PORT) {
@@ -23,5 +18,3 @@ if(!process.env.PORT && !process.env.C9_PORT) {
   util.log("Trying to start your browser in: " + url + "\n");
   require("child_process").exec("x-www-browser " + url);
 }
-=======
->>>>>>> 706fd55495135a8c84a331e275e241145decf878
